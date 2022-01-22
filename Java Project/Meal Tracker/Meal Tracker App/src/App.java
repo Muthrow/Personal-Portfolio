21744dd3535e5cc93b07ed46a9a116cf196af8ff
Zack Pedersen <zackpedersen526@gmail.com>
2022-01-22 01:42:54 -0700

    Second Commit

--- Java Project/Meal Tracker/Meal Tracker App/src/App.java
+++ Java Project/Meal Tracker/Meal Tracker App/src/App.java
@@ -1,10 +1,28 @@
 import java.io.File;
 import java.io.IOException;
-
+import java.util.Scanner;
+import FoodItem.FoodItem;
+JSON.simple.jar
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("What would you like to do?\n1. New day entry\n2. View previous day\n3. View nutrition facts");
-        switch
+        Scanner input = new Scanner(System.in);
+        System.out.println("What would you like to do?\n\n1. New day entry\n2. View previous day\n3. View nutrition facts");
+
+
+        // Receive user input and execute choice
+        int choice = input.nextInt();
+        switch(choice) {
+            case 1:
+            // new day
+            case 2:
+            // Prompt for day
+            // View previous day
+            case 3:
+            // Prompt for food
+            // Show food
+            default:
+            System.out.println("Invalid. \nPlease choose a valid option");
+        }
     }
 }
